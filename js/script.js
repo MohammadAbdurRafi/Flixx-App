9fbdc67fe602b146493884cf12b3682f7bb46aa4
Mohammad Abdur Rafi <mohammad.abdur.rafi.2@gmail.com>
2023-08-28 20:56:03 -0400

    Search Function & Custom Alert

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 const global = {
-    currentPage: window.location.pathname
+    currentPage: window.location.pathname,
+    search: {
+        term : '',
+        type: '',
+        page: 1,
+        totalPages: 1
+    },
+    api: {
+        apiKey: '3672298430a23e5e03b12e1a7a0e39da',
+        apiUrl: 'https://api.themoviedb.org/3/'
+    }
 }
 
 // Fetching and displaying popular movies
@@ -241,6 +251,22 @@ function displayBackgroundImage(type, backgroundPath) {
     }
 }
 
+// Search Movies/Shows
+async function search() {
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString)
+    
+    global.search.type = urlParams.get('type')
+    global.search.term = urlParams.get('search-term')
+
+    if (global.search.term !== '' && global.search.term !== null) {
+        const results = await searchAPIData()
+        console.log(results);
+    } else {
+        showAlert('Please enter a search term')
+    }
+}
+
 // Display Slider Movies
 async function displaySlider() {
     const { results } = await fetchAPIData('movie/now_playing')
@@ -291,12 +317,27 @@ function initSwiper() {
 
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
-    const API_KEY = '3672298430a23e5e03b12e1a7a0e39da';
-    const API_URL = 'https://api.themoviedb.org/3/'
+    const API_KEY = global.api.apiKey;
+    const API_URL = global.api.apiUrl
 
     showSpinner()
 
     const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`)
+    const data = await response.json()
+
+    hideSpinner()
+    
+    return data;
+}
+
+// Make Request To Search
+async function searchAPIData() {
+    const API_KEY = global.api.apiKey;
+    const API_URL = global.api.apiUrl
+
+    showSpinner()
+
+    const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`)
     const data = await response.json()
 
     hideSpinner()
@@ -324,6 +365,16 @@ function highlightActiveLink() {
     })
 }
 
+// Show Alert
+function showAlert(message, className) {
+    const alertEl = document.createElement('div')
+    alertEl.classList.add('alert', className)
+    alertEl.appendChild(document.createTextNode(message))
+    document.querySelector('#alert').appendChild(alertEl)
+
+    setTimeout(() => alertEl.remove(),3000)
+}
+
 // Init app 
 function init() {
     switch (global.currentPage) {
@@ -342,7 +393,7 @@ function init() {
             displayShowDetails()
             break;
         case '/search.html':
-            console.log('Search');
+            search()
             break;
     }
 
